d1c58173c27dfabdb9f0a730c24584edbe28d72e
abubakkar32 <abu35-1994@diu.edu.bd>
2023-08-14 11:37:56 +0600

    course model added

--- project/course/models.py
+++ project/course/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class course(models.Model):
+class Course(models.Model):
     course_name = models.CharField(max_length=100)
     course_code = models.CharField(max_length=100)
     course_duration = models.CharField(max_length=100)
@@ -9,5 +9,12 @@ class course(models.Model):
     course_description = models.TextField()
     course_image = models.ImageField(upload_to='course/images', default='')
 
+    class Meta:
+        abstract = True
+
     def __str__(self):
         return self.course_name
+
+class fees(Course):
+
+
