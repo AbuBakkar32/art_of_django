46be8941dbe013d576f62ea2d3944fbf6b06cf7c
abubakkar32 <abu35-1994@diu.edu.bd>
2023-08-18 10:00:16 +0600

    added timestamps abstract class

--- project/course/models.py
+++ project/course/models.py
@@ -1,17 +1,20 @@
 from django.db import models
 
 # Create your models here.
-class Course(models.Model):
+class Timestamp(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract=True
+
+class Course(Timestamp):
     course_name = models.CharField(max_length=100)
     course_code = models.CharField(max_length=100)
     course_duration = models.CharField(max_length=100)
     course_fee = models.CharField(max_length=100)
     course_description = models.TextField()
     course_image = models.ImageField(upload_to='course/images', default='')
-
-    class Meta:
-        abstract = True
-        db_table = 'course'
 
     def __str__(self):
         return self.course_name
