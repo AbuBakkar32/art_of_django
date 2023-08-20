bf88293f24b6c6c270a17ccb5bcc566e29f834ef
abubakkar32 <abu35-1994@diu.edu.bd>
2023-08-20 21:19:31 +0600

    add student and enquiry

--- mako/script.py
+++ mako/script.py
@@ -2,6 +2,5 @@ from mako.template import Template
 from mako.lookup import TemplateLookup
 
 mylookup = TemplateLookup(directories=['.'])
-
 mytemplate = Template(filename='person.txt', lookup=mylookup)
 print(mytemplate.render())
--- project/course/models.py
+++ project/course/models.py
@@ -18,3 +18,22 @@ class Course(Timestamp):
 
     def __str__(self):
         return self.course_name
+
+class Student(Timestamp):
+    student_name = models.CharField(max_length=100)
+    student_email = models.CharField(max_length=100)
+    student_mobile = models.CharField(max_length=100)
+    student_address = models.TextField()
+    student_course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.student_name
+
+class Enquiry(Timestamp):
+    enquiry_name = models.CharField(max_length=100)
+    enquiry_email = models.CharField(max_length=100)
+    enquiry_mobile = models.CharField(max_length=100)
+    enquiry_message = models.TextField()
+
+    def __str__(self):
+        return self.enquiry_name
