0b5d21b1c07692ffea580832bd82a51b8a905bda
abubakkar32 <abu35-1994@diu.edu.bd>
2023-08-23 14:23:16 +0600

    add student and enquiry

--- project/course/models.py
+++ project/course/models.py
@@ -8,32 +8,32 @@ class Timestamp(models.Model):
     class Meta:
         abstract=True
 
-class Course(Timestamp):
-    course_name = models.CharField(max_length=100)
-    course_code = models.CharField(max_length=100)
-    course_duration = models.CharField(max_length=100)
-    course_fee = models.CharField(max_length=100)
-    course_description = models.TextField()
-    course_image = models.ImageField(upload_to='course/images', default='')
-
-    def __str__(self):
-        return self.course_name
-
-class Student(Timestamp):
-    student_name = models.CharField(max_length=100)
-    student_email = models.CharField(max_length=100)
-    student_mobile = models.CharField(max_length=100)
-    student_address = models.TextField()
-    student_course = models.ForeignKey(Course, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.student_name
-
-class Enquiry(Timestamp):
-    enquiry_name = models.CharField(max_length=100)
-    enquiry_email = models.CharField(max_length=100)
-    enquiry_mobile = models.CharField(max_length=100)
-    enquiry_message = models.TextField()
-
-    def __str__(self):
-        return self.enquiry_name
+# class Course(Timestamp):
+#     course_name = models.CharField(max_length=100)
+#     course_code = models.CharField(max_length=100)
+#     course_duration = models.CharField(max_length=100)
+#     course_fee = models.CharField(max_length=100)
+#     course_description = models.TextField()
+#     course_image = models.ImageField(upload_to='course/images', default='')
+#
+#     def __str__(self):
+#         return self.course_name
+#
+# class Student(Timestamp):
+#     student_name = models.CharField(max_length=100)
+#     student_email = models.CharField(max_length=100)
+#     student_mobile = models.CharField(max_length=100)
+#     student_address = models.TextField()
+#     student_course = models.ForeignKey(Course, on_delete=models.CASCADE)
+#
+#     def __str__(self):
+#         return self.student_name
+#
+# class Enquiry(Timestamp):
+#     enquiry_name = models.CharField(max_length=100)
+#     enquiry_email = models.CharField(max_length=100)
+#     enquiry_mobile = models.CharField(max_length=100)
+#     enquiry_message = models.TextField()
+#
+#     def __str__(self):
+#         return self.enquiry_name
