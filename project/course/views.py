7d860c5724668507049c1fc68ab389504ed4b3fe
abubakkar32 <abu35-1994@diu.edu.bd>
2023-08-26 11:41:42 +0600

    add student and enquiry

--- project/course/views.py
+++ project/course/views.py
@@ -1,26 +1,26 @@
-# from rest_framework.views import APIView
-# from rest_framework.response import Response
-# from rest_framework import status
-# from rest_framework_jwt.views import ObtainJSONWebToken
-# from django.contrib.auth.models import User
-#
-#
-# class CustomObtainJWTView(ObtainJSONWebToken):
-#     def post(self, request, *args, **kwargs):
-#         response = super().post(request, *args, **kwargs)
-#         if response.status_code == status.HTTP_200_OK:
-#             token = response.data['token']
-#             user = self.user
-#             user_data = {
-#                 'user_id': user.id,
-#                 'username': user.username,
-#             }
-#             response.data = {
-#                 'token': token,
-#                 'user': user_data,
-#             }
-#         return response
-#
-#
-#
-#
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_jwt.views import ObtainJSONWebToken
+from django.contrib.auth.models import User
+
+
+class CustomObtainJWTView(ObtainJSONWebToken):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        if response.status_code == status.HTTP_200_OK:
+            token = response.data['token']
+            user = self.user
+            user_data = {
+                'user_id': user.id,
+                'username': user.username,
+            }
+            response.data = {
+                'token': token,
+                'user': user_data,
+            }
+        return response
+
+
+
+
