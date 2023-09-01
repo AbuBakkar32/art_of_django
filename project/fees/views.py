a024be38a7c4ec1283496751b5523493c00a5685
abubakkar32 <abu35-1994@diu.edu.bd>
2023-09-01 10:12:13 +0600

    edit templates

--- project/fees/views.py
+++ project/fees/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from django.shortcuts import get_object_or_404, redirect
 from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import render
 
 
+def index(request):
+    return render(request, 'fees/index.html')
