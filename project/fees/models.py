f3bb037024b00f37f26656194128742486f8c18c
abubakkar32 <abu35-1994@diu.edu.bd>
2023-09-02 13:02:27 +0600

    edit templates

--- project/fees/models.py
+++ project/fees/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Timestamp(models.Model):
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Student(Timestamp):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.IntegerField()
+    address = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Fees(Timestamp):
+    name = models.CharField(max_length=100)
+    amount = models.IntegerField()
+    date = models.DateField()
+    status = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
