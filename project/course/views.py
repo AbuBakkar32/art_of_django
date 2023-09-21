d330e5b2264f191a18ae7dab250dc81dec95549d
ABS RAKIB <abu35-1994@diu.edu.bd>
2023-09-21 09:21:25 +0600

    views.py file modified

--- project/course/views.py
+++ project/course/views.py
@@ -4,3 +4,11 @@ from django.http import HttpResponse
 def index(request, name):
     a = f"<h1>Hello World! Im {name}</h1>"
     return HttpResponse(a)
+
+
+def about(request):
+    return HttpResponse("<h1>About Page</h1>")
+
+
+def contact(request):
+    return HttpResponse("<h1>Contact Page</h1>")
